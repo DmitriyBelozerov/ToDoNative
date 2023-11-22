9b1c46d479768029653a8dc1ae3b5a26c74b852b
Dmitriy Belozerov <dm.belozerov@yandex.ru>
2023-11-22 21:02:38 +0300

    init

---
+++ todo-native/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text >Добрый !</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  h1: {
+    color: '#000',
+  }
+});
